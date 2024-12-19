e64c5b7361388e7e292aa2aa4855463c5f91a88b
Can Aydemir <canaydemir84@gmail.com>
2024-12-19 12:31:38 +0300

    Delete program/menu.php

--- program/menu.php
+++
@@ -1,7 +0,0 @@
-  <ul>
-    <li><a href="index.html">Sınıf Ders Programı (Öğretmensiz)</a></li>
-    <li><a href="index2.html">Sınıf Ders Programı (Öğretmeli)</a></li>
-    <li><a href="ogretmen.html">Öğretmen Ders Programı</a></li>
-    <li><a href="derslik.html">Derslik Ders Programı</a></li>
-    <li><a href="kamp.html">Ocak 2025 Kamp Ders Programı</a></li>
-  </ul> 
